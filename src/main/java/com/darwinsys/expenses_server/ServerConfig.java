21f9279995e2042a5625fc6fd1c8eb3e6ec3c475
Ian Darwin <ian@darwinsys.com>
2022-09-02 13:26:11 -0400

    Trying to get http and https both, not there yet...
    Using https://stackoverflow.com/questions/30896234/how-set-up-spring-boot-to-run-https-http-ports#36853652

---
+++ src/main/java/com/darwinsys/expenses_server/ServerConfig.java
@@ -0,0 +1,23 @@
+package com.darwinsys.expenses_server;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+@Component
+public class ServerConfig {
+
+        @Bean
+        public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort) {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setPort(httpPort);
+            System.out.println("ServletWebServerVactory: added port = " + httpPort);
+            TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+            tomcat.addAdditionalTomcatConnectors(connector);
+            return tomcat;
+        }
+
+}
